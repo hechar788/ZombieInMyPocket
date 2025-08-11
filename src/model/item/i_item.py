3a26d4e4643b9c0c574f4cf3d7819bdab52e838d
hechar788 <149678618+hechar788@users.noreply.github.com>
2025-08-12 11:09:08 +1200

    why so serious

--- src/model/item/i_item.py
+++ src/model/item/i_item.py
@@ -1,2 +1,39 @@
-class IItem:
-    pass
+from abc import ABC, abstractmethod
+from typing import List
+
+class IItem(ABC):
+    """Abstract interface defining the contract for item objects in the game."""
+    
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+    
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        pass
+    
+    @property
+    @abstractmethod
+    def type(self) -> str:
+        pass
+    
+    @property
+    @abstractmethod
+    def attack_bonus(self) -> int:
+        pass
+    
+    @property
+    @abstractmethod
+    def heal_amount(self) -> int:
+        pass
+    
+    @property
+    @abstractmethod
+    def is_single_use(self) -> bool:
+        pass
+    
+    @abstractmethod
+    def combinable_with(self) -> List[str]:
+        pass
