2467bf0a94b83e6609b2e29e86bb212ccc1eaf0d
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-08-13 02:15:19 +1200

    game manager file created

---
+++ src/model/game-manager/game_manager.py
@@ -0,0 +1,14 @@
+class GameManager:
+    """ """
+    def __init__(self):
+        """ Coordinates the state and the components of the game (Time, Turn, Player, and GameOver/GameStatus)"""
+        pass
+
+    def setup_game(self):
+        pass
+
+    def end_game(self):
+        pass
+
+    def reset_game(self):
+        pass
