3d81c35cc2b42c602c9013474f9cd6be14f7e853
David Watts <davidthomaswatts@gmail.com>
2025-08-15 10:11:26 +1200

    added more item test cases

--- src/model/item/test/test_can_of_soda.py
+++ src/model/item/test/test_can_of_soda.py
@@ -3,18 +3,18 @@ from ..item_helper import IItem, get_item
 from enums_and_types import ItemName, ItemType
 
 
-class TestGasoline(TestCase):
+class TestCanOfSoda(TestCase):
 
     def setUp(self):
-        self._item: IItem = get_item(ItemName.GASOLINE)
+        self._item: IItem = get_item(ItemName.CAN_OF_SODA)
 
-    def test_name_is_gasoline(self):
-        expected = ItemName.GASOLINE
+    def test_name_is_can_of_soda(self):
+        expected = ItemName.CAN_OF_SODA
         actual = self._item.name
         self.assertEqual(expected, actual)
-    
-    def test_type_is_combine_only(self):
-        expected = ItemType.COMBINE_ONLY
+
+    def test_type_is_healing(self):
+        expected = ItemType.HEALING
         actual = self._item.type
         self.assertEqual(expected, actual)
 
@@ -29,8 +29,12 @@ class TestGasoline(TestCase):
         actual = self._item.uses_remaining
         self.assertEqual(expected, actual)
 
-    def test_combinable_with_candle(self):
-        self.assertIn(ItemName.CANDLE, self._item.combinable_with)
+    def test_combinable_with_nothing(self):
+        expected = []
+        actual = self._item.combinable_with
+        self.assertEqual(expected, actual)
 
-    def test_combinable_with_chainsaw(self):
-        self.assertIn(ItemName.CHAINSAW, self._item.combinable_with)
+    def test_health_bonus_is_two(self):
+        expected = 2
+        actual = self._item.heal_amount
+        self.assertEqual(expected, actual)
