085d1a7c0d93bef3f43fbceb9382c7f069be6469
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-08-15 09:26:03 +1200

    message interface get_game_status

--- src/model/get_game_status/get_game_status.py
+++ src/model/get_game_status/get_game_status.py
@@ -1,38 +1,40 @@
-from ..interfaces.i_get_game_status import IGameStatus
-from ...enums_and_types.enums import GameStateMessage, GameInstruction, AlertMessage, UnknownErrorMessage
+from abc import ABC
+from typing import Optional
 
-class GameStatus(IGameStatus):
-    def __init__(self) -> None:
-        # self._is_game_over: bool = False
-        self._game_over_message: str | None = None
-        self._system_message: str | None = None
+from ..interfaces.i_get_game_status import IGameStatus
+from ..interfaces.i_time import ITime
+from ...enums_and_types.enums import GameStateMessage, GameInstruction, AlertMessage, UnknownErrorMessage, GameState, \
+    GameOverMessage
+
+
+class GameStatus(IGameStatus, ABC):
+    def __init__(self, current_time: ITime) -> None:
+        self._game_over_message = None
+        self.current_time = current_time
+        self._state = GameState.INIT
+        self._low_health = False
 
     # @property
-    # def is_game_over(self) -> bool:
-    #     return self._is_game_over
-
+    # def check_game_state(self) -> GameState:
+    #     """"""
+    #     return self._state
 
     @property
-    def handle_game_over(self) -> str | None:
+    def handle_game_over(self) -> Optional[GameOverMessage]:
+        """ Retrieves message code for the end game details, based on GameOverMessage enum"""
         return self._game_over_message
 
     @property
-    def system_message(self) -> str | None:
-        return self._system_message
-
-    #TODO: Change condition type to enum of game over conditions
-    def trigger_game_over(self, condition: str) -> None:
-        # self._is_game_over = True
-        self._game_over_message = condition
-
-    def reset(self) -> None:
-        """Resets for new game."""
-        # self._is_game_over = False
-        self._game_over_message = None
-        self._system_message = None
-
-    def post_message(self, code: GameStateMessage, *args) -> None:
-        # TODO: Add validation here for if args are missing but required
+    def get_state_message(self) -> Optional[GameStateMessage]:
+        """Retrieves message code that constantly updates based on the players choices (i.e. room change, item selection/acquired, heal, and attack score gains  """
         pass
-        # message_template = self._MESSAGE_MAP.get(code, "Unknown system event.")
-        # self._system_message = message_template.format(*args) # Error Possible Here
+
+    @property
+    def handle_help_key(self) -> Optional[GameInstruction]:
+        """ Retrieves message code that corresponds to the current room and game state when H key is pressed"""
+        pass
+
+    @property
+    def handle_game_warning_event(self) -> Optional[AlertMessage]:
+        """ Retrieves message code when triggered by game time warnings"""
+        pass
--- src/model/interfaces/i_get_game_status.py
+++ src/model/interfaces/i_get_game_status.py
@@ -1,23 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
+
 from ...enums_and_types.enums import GameStateMessage, GameInstruction, AlertMessage, UnknownErrorMessage, GameState, \
     GameOverMessage
-
 
 class IGameStatus(ABC):
     """ Handles game status messages."""
 
-    @property
-    @abstractmethod
-    def check_game_state(self) -> GameState:
-        pass
+    # @abstractmethod
+    # def check_game_state(self) -> GameState:
+    #     pass
 
-    @property
     @abstractmethod
     # TODO: Change condition type to enum of game over conditions
     def handle_game_over(self) -> Optional[GameOverMessage]:
         """ Game over event-driven that calls appropriate game over message and options, based on GameOverMessage enum"""
+        pass
+
+    @abstractmethod
+    def get_current_time(self) -> int:
+        # TODO: use time components to get it.
         pass
 
     @abstractmethod
@@ -30,7 +33,6 @@ class IGameStatus(ABC):
         """ Toggles show/hide instruction when H key is pressed, based on GameInstruction enum"""
         pass
 
-    @property
     @abstractmethod
     def handle_game_warning_event(self) -> Optional[AlertMessage]:
         """ Handles event-driven alerts (e.g. almost time, low health, invalid moves) and informs users based on AlertMessage enum"""
